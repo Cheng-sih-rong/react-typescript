ef6c81d29e61f67cc76de642c5e8548124810c8b
成思蓉 <sihrong.cheng@leishan.com.tw>
2021-09-28 14:28:29 +0800

    first commit

---
+++ src/contanier/NormalModal.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NormalModal.css';
+import Button from '../component/Button';
+
+function Modal() {
+  return (
+    <div className="App">
+      <div className="contanier"> <Button /></div>
+    </div>
+  );
+}
+
+export default Modal;
---
+++ src/page/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Modal from '../contanier/NormalModal';
+
+function App() {
+  return (
+    <div className="App">
+      <Modal />
+    </div>
+  );
+}
+
+export default App;
