bb7bbbf24f5674e71799e172a8af98e3e1e0732f
成思蓉 <sihrong.cheng@leishan.com.tw>
2021-09-29 09:51:09 +0800

    feat:Typescript use

--- src/contanier/NormalModal.tsx
+++ src/contanier/NormalModal.tsx
@@ -3,11 +3,14 @@ import './NormalModal.css';
 
 
 function Modal() {
+  
   const [count,setCount] = useState(1)
+  const title:string='年度成就'
+
   return (
     <div className="App">
       <div className="contanier center"> 
-        <h2>年度成就</h2>
+        <h2> {title}</h2>
         <p>{count}</p>
         <button onClick={()=>count< 5?setCount(count+1):setCount(1)}> Next </button>
       </div>
